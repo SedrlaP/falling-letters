f6fe1c71ac31d596397824e93e9472f39fcfbbdf
sedrlaP <prokop.sedrla@gmail.com>
2025-02-09 00:44:41 +0100

    Enhance score text display by adding background and container for score number

--- src/createScoreText.ts
+++ src/createScoreText.ts
@@ -1,4 +1,4 @@
-import { Application, Text } from 'pixi.js';
+import { Application, Container, Graphics, Text } from 'pixi.js';
 
 export function createScoreText(
   app: Application, 
@@ -7,9 +7,33 @@ export function createScoreText(
   MENU_Y: number,
   SCORE_TEXT_HEIGHT_OFFSET: number,
 ) {
-    const scoreText = new Text({ text: 'Score: ' + score, 
+    const scoreText = new Container();
+
+    const scoreString = new Text({ text: 'Score: ', 
         style: { fontSize: 16} 
       });
-    scoreText.position.set(app.screen.width - (MENU_WIDTH + scoreText.width) / 2, SCORE_TEXT_HEIGHT_OFFSET + MENU_Y)
+    scoreString.position.set(0, 7.5)
+
+    const scoreNumberContainer = new Container();
+
+    const scoreNumber = new Text({ text: score, 
+      style: { fontSize : 16}
+    });
+    scoreNumber.anchor.set(0.5);
+    scoreNumber.position.set(15, 15)
+
+    const scoreNumberBg = new Graphics();
+    scoreNumberBg.rect(0, 0, 30, 30);
+    scoreNumberBg.fill(0xFFFFFF);
+    scoreNumberBg.stroke({ width: 2, color: 0x000000 });
+
+    scoreText.addChild(scoreString);
+    scoreText.addChild(scoreNumberContainer);
+    scoreNumberContainer.addChild(scoreNumberBg);
+    scoreNumberContainer.addChild(scoreNumber);
+
+    scoreNumberContainer.position.set(scoreText.width, 0)
+    scoreText.position.set(app.screen.width - (MENU_WIDTH + scoreText.width) / 2, SCORE_TEXT_HEIGHT_OFFSET + MENU_Y + 5)
+    
     return scoreText;
 }
