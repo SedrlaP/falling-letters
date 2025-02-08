f619db103950ff905798e1ac04bf39d4dd306710
sedrlaP <prokop.sedrla@gmail.com>
2025-02-09 00:02:56 +0100

    Fix using deprecated code

--- src/createRectangle.ts
+++ src/createRectangle.ts
@@ -1,10 +1,9 @@
-import { Application, Graphics, Text}  from "pixi.js";
+import { Application, Container, Graphics, Text}  from "pixi.js";
 
 export function createRectangle(app: Application, menuWidth: number) {
     // Generate random rectangle height and width
     const sideLength = Math.floor(Math.random() * 100) + 20;
-    const rectangle = new Graphics();
-
+    
     // Get text coordinates from side length
     const textCoordinates = sideLength / 2;
 
@@ -16,11 +15,14 @@ export function createRectangle(app: Application, menuWidth: number) {
     const randomIndex = Math.floor(Math.random() * letters.length); // Get a random index
     const randomLetter = letters[randomIndex]; // Get the random letter
 
+    const rectangle = new Container();
+    const rectangleBg = new Graphics();
     const text = new Text({ text: randomLetter, style: { fontSize: fontSize}});
     text.anchor.set(0.5);
     text.position.set(textCoordinates, textCoordinates);
-    rectangle.rect(0, 0, sideLength, sideLength);
-    rectangle.fill(0x9a9a9a);
+    rectangleBg.rect(0, 0, sideLength, sideLength);
+    rectangleBg.fill(0x9a9a9a);
+    rectangle.addChild(rectangleBg);
     rectangle.addChild(text);
 
     // Set rectangle position where x is random and y is 0
