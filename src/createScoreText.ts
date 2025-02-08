72c82a7382a8af68d54eba07fcd7433379404750
sedrlaP <prokop.sedrla@gmail.com>
2025-02-08 17:32:50 +0100

    Refactor createScoreText to accept MENU_Y parameter for dynamic vertical positioning

--- src/createScoreText.ts
+++ src/createScoreText.ts
@@ -1,9 +1,15 @@
 import { Application, Text } from 'pixi.js';
 
-export function createScoreText(app: Application, score: number, MENU_WIDTH: number, SCORE_TEXT_HEIGHT_OFFSET: number) {
+export function createScoreText(
+  app: Application, 
+  score: number, 
+  MENU_WIDTH: number, 
+  MENU_Y: number,
+  SCORE_TEXT_HEIGHT_OFFSET: number,
+) {
     const scoreText = new Text({ text: 'Score: ' + score, 
         style: { fontSize: 16} 
       });
-    scoreText.position.set(app.screen.width - MENU_WIDTH, SCORE_TEXT_HEIGHT_OFFSET)
+    scoreText.position.set(app.screen.width - MENU_WIDTH, SCORE_TEXT_HEIGHT_OFFSET + MENU_Y)
     return scoreText;
 }
