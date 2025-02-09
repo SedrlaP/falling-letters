5c7a148e7da3860995375c3dc67235220f05aba7
sedrlaP <prokop.sedrla@gmail.com>
2025-02-09 01:02:13 +0100

    Refactor score handling functions to use getChildByName for improved readability

--- src/handleScore.ts
+++ src/handleScore.ts
@@ -2,7 +2,7 @@ import {Container, Text}  from "pixi.js";
 
 export function increaseScore(score: number, scoreText: Container) {
     score += 1;
-    (scoreText.children[1].children[1] as Text).text = score;
+    (scoreText.children[1].getChildByName('scoreNumber') as Text).text = score;
     
     return score
 }
@@ -11,13 +11,13 @@ export function decreaseScore(score: number, scoreText: Container) {
     score -= 2;
     // If score is less than 0 return 0 else return score
     score = score <= 0 ? 0 : score;
-    (scoreText.children[1].children[1] as Text).text = score;
+    (scoreText.children[1].getChildByName('scoreNumber') as Text).text = score;
     return score
 }
   
 export function resetScore(score: number, scoreText: Container) {
     score = 0;
-    (scoreText.children[1].children[1] as Text).text = score;
+    (scoreText.children[1].getChildByName('scoreNumber') as Text).text = score;
 
     return score
 }
